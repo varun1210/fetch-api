3e8b4283f6e222311d5754e8d478f8e51824d7f9
Varun Muppalla <varunm1210@gmail.com>
2023-10-19 02:56:25 -0400

    code cleanup

--- general_utils/errorHandler.js
+++ general_utils/errorHandler.js
@@ -1,15 +1,15 @@
-const { BadRequestError, BaseURLError } = require("./errors")
+const { BadRequestError, BaseURLError } = require("./errors");
 
 const errorHandler = (error, req, res, next) => {
-    if(error  instanceof BadRequestError) {
-        res.status(400).send({"ERROR" : error.message})
-    } else if (error instanceof BaseURLError){
-        res.status(404).send({"ERROR" : error.message})
-    } else if (error.message == "No resource found error") {
-        res.status(404).send({"ERROR" : "No resource found!"})
-    }else {
-        res.status(500).send({"ERROR" : "Internal server error!"})
-    }
-} 
+  if (error instanceof BadRequestError) {
+    res.status(400).send({ ERROR: error.message });
+  } else if (error instanceof BaseURLError) {
+    res.status(404).send({ ERROR: error.message });
+  } else if (error.message == "No resource found error") {
+    res.status(404).send({ ERROR: "No resource found!" });
+  } else {
+    res.status(500).send({ ERROR: "Internal server error!" });
+  }
+};
 
 module.exports = errorHandler;
