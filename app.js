9196eec85ebed68bce10686094689a756746a32f
Varun Muppalla <varunm1210@gmail.com>
2023-10-19 03:09:28 -0400

    code cleanup

--- app.js
+++ app.js
@@ -1,87 +1,88 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
+const errors = require("./general_utils/errors");
+const errorHandler = require("./general_utils/errorHandler");
 
-const errors = require('./general_utils/errors')
-const errorHandler = require('./general_utils/errorHandler')
-
-
-const generateId = require('./receipt_utils/idGenerator')
-const validateReceipt = require('./receipt_utils/receiptValidator');
-const scoreCalculator = require('./receipt_utils/scoreCalculator');
-
+const generateId = require("./receipt_utils/idGenerator");
+const validateReceipt = require("./receipt_utils/receiptValidator");
+const scoreCalculator = require("./receipt_utils/scoreCalculator");
 
 const port = 3000;
 const app = express();
 
-
 const receipts = new Map();
-const invalidRoute = /^\/receipts/
+const invalidRoute = /^\/receipts/;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({'extended' : false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/receipts/process', (req, res, next) => {
-    try {
-        if(Object.keys(req.body).length == 0) {
-            throw new errors.BadRequestError("INVALID RECEIPT: Received invalid format OR empty receipt")
-        }
-        let receipt = req.body;
-        let receiptValFlag = validateReceipt(receipt)
-        let receiptPoints = scoreCalculator(receipt)
-        if(receiptValFlag instanceof errors.BadRequestError) {
-            throw receiptValFlag
-        }
-        if(receiptPoints instanceof Error) {
-            throw new errors.BadRequestError("INVALID RECEIPT: One or more of the fields in this receipt is inconsistent")
-        }
-        receipt.points = receiptPoints
-        let id = generateId();
-        while(receipts.has(id)) {
-            id = generateId();
-        }
-        receipts.set(id, receipt);
-        res.status(200).send({ "id" : id });
-    } catch (error) {
-        next(error);
+app.post("/receipts/process", (req, res, next) => {
+  try {
+    if (Object.keys(req.body).length == 0) {
+      throw new errors.BadRequestError(
+        "INVALID RECEIPT: Received invalid format OR empty receipt"
+      );
     }
-})
-
-app.get('/receipts/:id/points', (req, res, next) => {
-    try {
-        let receiptID = req.params.id;
-        if(!receipts.has(receiptID)) {
-            throw new errors.BadRequestError("INVALID RECEIPT ID: Receipt ID does not exist")
-        } else {
-            receipt = receipts.get(receiptID);
-            res.status(200).send({ "points" : receipt.points })    
-        }
-    } catch (error) {
-        next(error);
+    let receipt = req.body;
+    let receiptValFlag = validateReceipt(receipt);
+    let receiptPoints = scoreCalculator(receipt);
+    if (receiptValFlag instanceof errors.BadRequestError) {
+      throw receiptValFlag;
     }
-})
+    if (receiptPoints instanceof Error) {
+      throw new errors.BadRequestError(
+        "INVALID RECEIPT: One or more of the fields in this receipt is inconsistent"
+      );
+    }
+    receipt.points = receiptPoints;
+    let id = generateId();
+    while (receipts.has(id)) {
+      id = generateId();
+    }
+    receipts.set(id, receipt);
+    res.status(200).send({ id: id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/receipts/:id/points", (req, res, next) => {
+  try {
+    let receiptID = req.params.id;
+    if (!receipts.has(receiptID)) {
+      throw new errors.BadRequestError(
+        "INVALID RECEIPT ID: Receipt ID does not exist"
+      );
+    } else {
+      receipt = receipts.get(receiptID);
+      res.status(200).send({ points: receipt.points });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.use((req, res, next) => {
-    try {
-        if(!req.url.match(/^\/receipts/)) {
-            throw new Error("No resource found error")
-        }
-        next();
-    } catch (error) {
-        next(error)
+  try {
+    if (!req.url.match(/^\/receipts/)) {
+      throw new Error("No resource found error");
     }
-})
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-app.route(/^\/receipts/)
-  .all((req, res, next) => {
-    try {
-        throw new errors.BaseURLError("Please hit a valid endpoint")
-    } catch (error) {
-        next(error)
-    }
-  })
+app.route(/^\/receipts/).all((req, res, next) => {
+  try {
+    throw new errors.BaseURLError("Please hit a valid endpoint");
+  } catch (error) {
+    next(error);
+  }
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
--- receipt_utils/receiptValidator.js
+++ receipt_utils/receiptValidator.js
@@ -1,24 +1,26 @@
-const { BadRequestError } = require("../general_utils/errors")
+const { BadRequestError } = require("../general_utils/errors");
 
 module.exports = validateReceipt = (receipt) => {
-    try {
-        if(!('retailer' in receipt) || (receipt.retailer == "")) {
-            throw new BadRequestError("INVALID RECEIPT: Retailer name required!")
-        }
-        if(!('purchaseDate' in receipt) || (receipt.purchaseDate == "")) {
-            throw new BadRequestError("INVALID RECEIPT: Purchase date required!")
-        }
-        if(!('purchaseTime' in receipt) || (receipt.purchaseDate == "")) {
-            throw new BadRequestError("INVALID RECEIPT: Purchase time required!")
-        }
-        if(!('items' in receipt)) {
-            throw new BadRequestError("INVALID RECEIPT: Purchase contains no items!")
-        }
-        if(!('total' in receipt)) {
-            throw new BadRequestError("INVALID RECEIPT: Purchase does not contain a total!")
-        }
-        return true;
-    } catch (error) {
-        return error;
-    } 
-}
+  try {
+    if (!("retailer" in receipt) || receipt.retailer == "") {
+      throw new BadRequestError("INVALID RECEIPT: Retailer name required!");
+    }
+    if (!("purchaseDate" in receipt) || receipt.purchaseDate == "") {
+      throw new BadRequestError("INVALID RECEIPT: Purchase date required!");
+    }
+    if (!("purchaseTime" in receipt) || receipt.purchaseDate == "") {
+      throw new BadRequestError("INVALID RECEIPT: Purchase time required!");
+    }
+    if (!("items" in receipt)) {
+      throw new BadRequestError("INVALID RECEIPT: Purchase contains no items!");
+    }
+    if (!("total" in receipt)) {
+      throw new BadRequestError(
+        "INVALID RECEIPT: Purchase does not contain a total!"
+      );
+    }
+    return true;
+  } catch (error) {
+    return error;
+  }
+};
